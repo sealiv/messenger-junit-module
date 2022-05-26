86db09a506adb327c4718e5b3e30571f48165f5c
Sealiv <sai.79.ua@gmail.com>
2022-05-26 16:56:02 +0300

    Added simple implementation of generateMessage() method with test.

--- src/main/java/com/epam/ld/module2/testing/template/Template.java
+++ src/main/java/com/epam/ld/module2/testing/template/Template.java
@@ -4,4 +4,14 @@ package com.epam.ld.module2.testing.template;
  * The type Template.
  */
 public class Template {
+
+    private final String text;
+
+    public Template(String text) {
+        this.text = text;
+    }
+
+    public String setTag(String key, String value) {
+        return text.replaceAll("#\\{" + key + "}", value);
+    }
 }
--- src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
+++ src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
@@ -1,4 +1,33 @@
 package com.epam.ld.module2.testing.template;
 
+import com.epam.ld.module2.testing.Client;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TemplateEngineTest {
+
+    private Template template;
+    private Client client = new Client();
+    private final TemplateEngine templateEngine = new TemplateEngine();
+    private static final String TEMPLATE_WITH_ONE_TAG = "Mail to: #{subject}\n" +
+            "**********************************************************************\n" +
+            "Dear Employee,\n" +
+            "Thank you for your recent resume and letter expressing your interest\n" +
+            "in working for our company.\n Will be send message to '#{subject}'";
+
+    @BeforeEach
+    void setUp() {
+        client = new Client();
+        client.setAddresses("abc@mail.com");
+    }
+
+    @Test
+    public void shouldChangeTagToValue_WhenGenerateMessage() {
+        template = new Template(TEMPLATE_WITH_ONE_TAG);
+        String actual = templateEngine.generateMessage(template, client);
+        assertTrue(actual.contains(client.getAddresses()));
+    }
 }
